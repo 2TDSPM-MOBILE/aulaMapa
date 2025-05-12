1e7462c6915afd100aaa7334af0ec015ee791ccd
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-05-12 11:57:26 -0300

    Realizado a exibição da latitude e longitude, falta renderizar o mapaview

--- App.js
+++ App.js
@@ -1,11 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+//Importando a biblioteca de localização
+import * as Location from 'expo-location'
+
+//Importando o mapa e o pin vermelho do mapa
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default function App() {
+  //Estado para armazenar a localização(latitude e longitude)
+  const [location, setLocation] = useState(null)
+
+  //Estado para armazenar o endereço
+  const [address, setAddress] = useState(null)
+
+  //Estado para armazenar o status da permissão
+  const [permission, setPermission] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      //Solicitar permissão para acessar a loc do dispositivo
+      const { status } = await Location.requestForegroundPermissionsAsync()
+      setPermission(status)// Armazena o status da permissão
+
+      //Se a permissão foi garantida
+      if (status === 'granted') {
+        //Obter localização atual do dispositivo
+        const userLocation = await Location.getCurrentPositionAsync({})
+        setLocation(userLocation.coords)//Salva no estado Location, a latitude e longitude
+
+        const addressResult = await Location.reverseGeocodeAsync(userLocation.coords)
+        setAddress(addressResult[0])//Armazena o endereço mais relevante
+      }
+    })()//Função assíncrona autoexecutável
+  }, [])
+
+  //Se a permissão da Loc não for concedida
+  if (permission !== 'granted') {
+    return (
+      <View>
+        <Text>Permissão de localização não concedida.</Text>
+        <Button title='Solicitar Permissão' onPress={() => {
+          (async () => {
+            //Solicitar permissão para acessar a loc do dispositivo
+            const { status } = await Location.requestForegroundPermissionsAsync()
+            setPermission(status)// Armazena o status da permissão
+          })()
+        }} />
+      </View>
+    )
+  }
+
+  //Função para exibir o endereço
+  const renderAddress = () =>{
+    if(!address) return <Text>Carregando endereço...</Text>
+
+    const street = address?.street || 'Rua não encontrada.'
+    const city = address?.city || 'Cidade não encontrada.'
+    const region = address?.region || 'Estado não encontrado.'
+    const country = address?.country || 'País não encontrado.'
+    const postalCode = address?.postalCode || 'CEP não encontrado.'
+
+    return (
+      <View>
+        <Text>Endereço completo:</Text>
+        <Text>Rua:{street}</Text>
+        <Text>Cidade:{city}</Text>
+        <Text>Estado:{region}</Text>
+        <Text>País:{country}</Text>
+        <Text>CEP:{postalCode}</Text>
+      </View>
+    )
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text>Sua localização</Text>
+     {location?(
+      <>
+        <Text>Minha Latitude:{location.latitude}</Text>
+        <Text>Minha Longitude:{location.longitude}</Text>
+
+        <MapView>
+          <Marker 
+          
+          />
+        </MapView>
+      </>
+     ):(
+      <Text>Carregando endereço...</Text>
+     )}
     </View>
   );
 }
@@ -14,7 +101,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
